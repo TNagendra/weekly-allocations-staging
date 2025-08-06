2f7012628d7efb2efc6c956a9866d83fce9db5f2
TNagendra <nagendratangeti4846@gmail.com>
2025-08-06 16:52:03 +0530

    Added live frontend url

--- server/src/app.js
+++ server/src/app.js
@@ -9,7 +9,7 @@ const port = process.env.PORT || 5000;
 app.use(cors({
   origin: [
     'http://localhost:3000',
-    'https://weeklyallocationstaging.netlify.app',
+    'https://weekly-allocations-prod.netlify.app',
     process.env.FRONTEND_URL
   ].filter(Boolean), // Remove undefined values
   credentials: true
